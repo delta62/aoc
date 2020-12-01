3478f687925cc0de40587429e4ba361a49491264
Sam Noedel <sam.noedel@gmail.com>
2020-11-30 22:47:03 -0800

    Day one part 2

--- src/d01.rs
+++ src/d01.rs
@@ -9,22 +9,47 @@ fn part1(input: &str) -> Vec<u32> {
 }
 
 #[aoc(day1, part1)]
-fn solve_part1(input1: &[u32]) -> u32 {
+fn solve_part1(input: &[u32]) -> u32 {
 
     // Sort the list in descending order
-    let mut input = vec![0; input1.len()];
-    input[..].copy_from_slice(input1);
-    input.sort_unstable_by_key(|x| u32::MAX - x);
+    let mut sorted = vec![0; input.len()];
+    sorted[..].copy_from_slice(input);
+    sorted.sort_unstable_by_key(|x| u32::MAX - x);
 
-    for big in input.iter() {
+    for big in sorted.iter() {
         // Walk in from the end of the list
-        for small in input.iter().rev() {
+        for small in sorted.iter().rev() {
             if big + small == TARGET {
                 return big * small;
             }
 
             if big + small > TARGET {
                 break;
+            }
+        }
+    }
+
+    panic!("Sum not found!");
+}
+
+#[aoc(day1, part2)]
+fn solve_part2(input: &[u32]) -> u32 {
+
+    // Sort the list in descending order
+    let mut sorted = vec![0; input.len()];
+    sorted[..].copy_from_slice(input);
+    sorted.sort_unstable_by_key(|x| u32::MAX - x);
+
+    for big in sorted.iter() {
+        for middle in sorted[1..].iter() {
+            for small in sorted.iter().rev() {
+                if big + middle + small == TARGET {
+                    return big * middle * small;
+                }
+
+                if big + middle + small > TARGET {
+                    break;
+                }
             }
         }
     }
@@ -41,5 +66,12 @@ mod tests {
         let input = vec![ 1721, 979, 366, 299, 675, 1456 ];
         let result = solve_part1(input.as_slice());
         assert_eq!(result, 514579);
+    }
+
+    #[test]
+    fn day1_ex2() {
+        let input = vec![ 1721, 979, 366, 299, 675, 1456 ];
+        let result = solve_part2(input.as_slice());
+        assert_eq!(result, 241861950);
     }
 }
