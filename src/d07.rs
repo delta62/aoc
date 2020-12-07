309cf9b5d0985768d626f6c86ccb5d7fc97b4c9d
Sam Noedel <sam.noedel@gmail.com>
2020-12-06 16:40:13 -0800

    Empty day 7 impl

---
+++ src/d07.rs
@@ -0,0 +1,29 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day7)]
+fn parse(input: &str) -> Vec<usize> {
+    Vec::new()
+}
+
+#[aoc(day7, part1)]
+fn solve_part1(input: &[usize]) -> usize {
+    42
+}
+
+#[aoc(day7, part2)]
+fn solve_part2(input: &[usize]) -> usize {
+    42
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+    }
+
+    #[test]
+    fn ex2() {
+    }
+}
