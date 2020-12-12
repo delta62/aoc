f91a09d92f00fe4765bee4391675b9a587b555e0
Sam Noedel <sam.noedel@gmail.com>
2020-12-12 14:00:09 -0800

    Day 12 cleanup

--- src/d12.rs
+++ src/d12.rs
@@ -1,5 +1,8 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use std::ops::Deref;
 use std::str::FromStr;
+
+enum Direction { N, W, S, E }
 
 enum Instruction {
     North(isize),
@@ -8,6 +11,59 @@ enum Instruction {
     West(isize),
     Forward(isize),
     Rotate(isize),
+}
+
+#[derive(Default)]
+struct Orientation {
+    degrees: isize,
+}
+
+impl Orientation {
+    fn add(&mut self, degrees: isize) {
+        self.degrees = if degrees < 0 { degrees + 360 } else { degrees } % 360;
+    }
+}
+
+impl Deref for Orientation {
+    type Target = isize;
+
+    fn deref(&self) -> &Self::Target {
+        &self.degrees
+    }
+}
+
+#[derive(Default)]
+struct Point {
+    x: isize,
+    y: isize,
+}
+
+impl Point {
+    fn translate(&mut self, dir: &Direction, distance: isize) {
+        match dir {
+            Direction::N => self.y += distance,
+            Direction::S => self.y -= distance,
+            Direction::E => self.x += distance,
+            Direction::W => self.x -= distance,
+        }
+    }
+
+    fn rotate(&mut self, degrees: isize) {
+        let degrees = if degrees < 0 { degrees + 360 } else { degrees };
+        let rotations = degrees / 90;
+
+        for _ in 0..rotations {
+            let x = self.x;
+            let y = self.y;
+
+            self.x = y;
+            self.y = -x;
+        }
+    }
+
+    fn manhattan(&self) -> usize {
+        (self.x.abs() + self.y.abs()) as usize
+    }
 }
 
 impl FromStr for Instruction {
@@ -31,93 +87,48 @@ impl FromStr for Instruction {
     }
 }
 
+impl From<Ship> for Point {
+    fn from(ship: Ship) -> Self {
+        ship.loc
+    }
+}
+
+#[derive(Default)]
 struct Ship {
-    east: isize,
-    north: isize,
-    orientation: isize,
+    loc: Point,
+    orientation: Orientation,
 }
 
 impl Ship {
-    fn new() -> Self {
-        let east = 0;
-        let north = 0;
-        let orientation = 0;
-        Self { east, north, orientation }
-    }
-
-    fn perform(&mut self, instr: &Instruction) {
+    fn step(&mut self, instr: &Instruction) {
         match instr {
-            Instruction::North(x)   => self.north += x,
-            Instruction::South(x)   => self.north -= x,
-            Instruction::East(x)    => self.east += x,
-            Instruction::West(x)    => self.east -= x,
+            Instruction::North(x)   => self.loc.y += x,
+            Instruction::South(x)   => self.loc.y -= x,
+            Instruction::East(x)    => self.loc.x += x,
+            Instruction::West(x)    => self.loc.x -= x,
             Instruction::Rotate(x)  => self.rotate(*x),
             Instruction::Forward(x) => self.forward(*x),
         }
     }
 
     fn rotate(&mut self, degrees: isize) {
-        let mut degrees = degrees;
-
-        if degrees < 0 {
-            degrees += 360;
-        }
-
-        self.orientation = (self.orientation + degrees) % 360;
+        self.orientation.add(degrees);
     }
 
     fn forward(&mut self, distance: isize) {
-        match self.orientation {
-            0   => self.east  += distance,
-            90  => self.north -= distance,
-            180 => self.east  -= distance,
-            270 => self.north += distance,
-            _   => panic!("Can't move at angle {}", self.orientation),
+        match *self.orientation {
+            0   => self.loc.x += distance,
+            90  => self.loc.y -= distance,
+            180 => self.loc.x -= distance,
+            270 => self.loc.y += distance,
+            x   => panic!("Can't move at angle {}", x),
         }
     }
 
-    fn move_towards(&mut self, waypoint: &Waypoint, units: isize) {
-        for _ in 0..units {
-            self.north += waypoint.north;
-            self.east += waypoint.east;
-        }
-    }
-}
-
-struct Waypoint {
-    north: isize,
-    east: isize,
-}
-
-impl Waypoint {
-    fn new(east: isize, north: isize) -> Self {
-        Self { east, north }
-    }
-
-    fn rotate_about(&mut self, degrees: isize) {
-        let mut degrees = degrees;
-        if degrees < 0 {
-            degrees += 360
-        }
-
-        let rotations = degrees / 90;
-
-        for _ in 0..rotations {
-            let n = self.north;
-            let e = self.east;
-
-            self.east = n;
-            self.north = -e;
-        }
-    }
-
-    fn translate(&mut self, instr: &Instruction) {
-        match instr {
-            Instruction::North(x) => self.north += x,
-            Instruction::South(x) => self.north -= x,
-            Instruction::East(x)  => self.east  += x,
-            Instruction::West(x)  => self.east  -= x,
-            _ => panic!("Unknown translation command"),
+    fn translate_times(&mut self, vec: &Point, times: isize) {
+        for _ in 0..times {
+            self.loc.y += vec.y;
+            self.loc.x += vec.x;
         }
     }
 }
@@ -131,30 +142,31 @@ fn parse(input: &str) -> Vec<Instruction> {
 }
 
 #[aoc(day12, part1)]
-fn solve_part1(input: &[Instruction]) -> isize {
-    let mut ship = Ship::new();
+fn solve_part1(input: &[Instruction]) -> usize {
+    let mut ship = Ship::default();
 
-    for instr in input {
-        ship.perform(instr);
-    }
+    input.iter().for_each(|instr| ship.step(instr));
 
-    ship.east.abs() + ship.north.abs()
+    Point::from(ship).manhattan()
 }
 
 #[aoc(day12, part2)]
-fn solve_part2(input: &[Instruction]) -> isize {
-    let mut ship = Ship::new();
-    let mut waypoint = Waypoint::new(10, 1);
+fn solve_part2(input: &[Instruction]) -> usize {
+    let mut ship = Ship::default();
+    let mut waypoint = Point { x: 10, y: 1 };
 
     for instr in input {
         match instr {
-            Instruction::Rotate(x) => waypoint.rotate_about(*x),
-            Instruction::Forward(x) => ship.move_towards(&waypoint, *x),
-            _ => waypoint.translate(&instr),
+            Instruction::Rotate(x)  => waypoint.rotate(*x),
+            Instruction::Forward(x) => ship.translate_times(&waypoint, *x),
+            Instruction::North(x)   => waypoint.translate(&Direction::N, *x),
+            Instruction::South(x)   => waypoint.translate(&Direction::S, *x),
+            Instruction::East(x)    => waypoint.translate(&Direction::E, *x),
+            Instruction::West(x)    => waypoint.translate(&Direction::W, *x),
         }
     }
 
-    ship.east.abs() + ship.north.abs()
+    Point::from(ship).manhattan()
 }
 
 #[cfg(test)]
