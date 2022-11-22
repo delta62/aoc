b08bc349dd55b9097e167d2f010d615345fd1f0b
Sam Noedel <sam.noedel@gmail.com>
2022-11-22 12:58:49 -0800

    Warning cleanup

--- src/aoc/day4.clj
+++ src/aoc/day4.clj
@@ -1,28 +1,20 @@
 (ns aoc.day4
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string])
+  (:import java.security.MessageDigest java.math.BigInteger))
 
-(import 'java.security.MessageDigest
-        'java.math.BigInteger)
-
-(defn- md5
-  "Calculate the md5 hash of some string
-   From https://gist.github.com/jizhang/4325757"
-  [input]
+(defn- md5 [s]
   (let [algo (MessageDigest/getInstance "MD5")
-        raw (.digest algo (.getBytes input))]
-   (format "%032x" (BigInteger. 1 raw))))
+        raw (.digest algo (.getBytes s))]
+    (format "%032x" (BigInteger. 1 raw))))
 
-(defn- starts-with-five-zeroes [input]
-  (string/starts-with? (md5 input) "00000"))
-
-(defn- starts-with-six-zeroes [input]
-  (string/starts-with? (md5 input) "000000"))
+(defn- starts-with-zeroes [n input]
+  (string/starts-with? (md5 input) (string/join (repeat n \0))))
 
 (defn- find-idx [f coll]
-  (first (keep-indexed #(if (f %2) %1) coll)))
+  (first (keep-indexed #(if (f %2) %1 nil) coll)))
 
 (defn part1 [input]
-  (find-idx starts-with-five-zeroes (map #(str input %1) (range))))
+  (find-idx #(starts-with-zeroes 5 %1) (map #(str input %1) (range))))
 
 (defn part2 [input]
-  (find-idx starts-with-six-zeroes (map #(str input %1) (range))))
+  (find-idx #(starts-with-zeroes 6 %1) (map #(str input %1) (range))))
