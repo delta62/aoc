63ec39d57cb40e8f92d0edddc28af9f82076bedc
Sam Noedel <sam.noedel@gmail.com>
2022-12-01 18:58:07 -0800

    Cleanup day 1 parse helper

--- src/aoc/day01.clj
+++ src/aoc/day01.clj
@@ -1,15 +1,17 @@
 (ns aoc.day01
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :refer [split split-lines]]))
 
 (defn- sum [nums]
   (apply + nums))
 
 (defn parse
-  "Parses a newline-separated string into nested vectors of numbers"
+  "Parses a newline-separated string into nested lists of numbers"
   [s]
-  (let [groups (string/split s #"\n\n")
-        to-nums (partial map parse-long)]
-    (map (comp to-nums string/split-lines) groups)))
+  (let [groups (split s #"\n\n")]
+    (map #(->> %1
+               (split-lines)
+               (map parse-long))
+         groups)))
 
 (defn part1 [input]
   (apply max (map sum input)))
