4167474dc08aeb6e5315490093924309a920881c
Sam Noedel <sam.noedel@gmail.com>
2022-12-01 18:47:45 -0800

    Use threading operator

--- src/aoc/day01.clj
+++ src/aoc/day01.clj
@@ -1,16 +1,22 @@
 (ns aoc.day01
   (:require [clojure.string :as string]))
 
-(defn parse [s]
+(defn- sum [nums]
+  (apply + nums))
+
+(defn parse
+  "Parses a newline-separated string into nested vectors of numbers"
+  [s]
   (let [groups (string/split s #"\n\n")
         to-nums (partial map parse-long)]
     (map (comp to-nums string/split-lines) groups)))
-
-(defn- sum [nums]
-  (apply + nums))
 
 (defn part1 [input]
   (apply max (map sum input)))
 
 (defn part2 [input]
-  (apply + (take 3 (sort > (map sum input)))))
+  (->> input
+       (map sum)
+       (sort >)
+       (take 3)
+       (sum)))
