061333946028b7ad9b00c5d91454c1cd7ab97eb6
Sam Noedel <sam.noedel@gmail.com>
2022-12-07 09:10:12 -0800

    docs

--- src/aoc/day07.clj
+++ src/aoc/day07.clj
@@ -1,26 +1,39 @@
 (ns aoc.day07
-  (:require [clojure.string :as string]
-            [aoc.util :refer [sum]]))
+  (:require [aoc.util :refer [sum]]
+            [clojure.string :as string]))
 
 (def total-space 70000000)
 (def needed-space 30000000)
 
-(defn parse-cd [path] {:command :cd, :path path})
+(defn parse-cd
+  "Parses a path into a :cd command object"
+  [path]
+  {:command :cd, :path path})
 
-(defn parse-ls-size [size]
-  (if (= "dir" size) :dir (parse-long size)))
+(defn parse-ls-size
+  "Given an entry's `ls` output, return the entry's size. Gives 0 for
+   directories."
+  [size]
+  (if (= "dir" size) 0 (parse-long size)))
 
-(defn parse-entry [state entry]
+(defn parse-entry
+  "Adds an `ls` entry into a hash of name/size pairs"
+  [state entry]
   (let [[size name] (string/split entry #"\s+")
         size (parse-ls-size size)]
     (assoc state name size)))
 
-(defn parse-ls [output]
+(defn parse-ls
+  "Parses output of an `ls` command into a :ls command object containing a hash
+   of name/size pairs"
+  [output]
   (let [entries (string/split-lines output)
         files (reduce parse-entry {} entries)]
     {:command :ls, :entries files}))
 
-(defn parse-cmd [line]
+(defn parse-cmd
+  "Parse a command's input & output into a command object"
+  [line]
   (let [[cmd output] (string/split line #"\s+" 2)]
     (case cmd
       "cd" (parse-cd output)
@@ -32,30 +45,47 @@
        (map string/trim)
        (map parse-cmd)))
 
-(defn update-path [path new-path]
+(defn update-path
+  "Given an existing path, calculates what the new path should be after running
+   `cd`"
+  [path new-path]
   (case new-path
     "/" "/"
     ".." (string/replace path #"\/[^\/]+\/[^\/]*$" "/")
     (str (string/join "" [path new-path]) "/")))
 
-(defn path-sequence [path]
+(defn path-sequence
+  "Splits a path into a sequence of components"
+  [path]
   (->> (string/split path #"\/")
        (rest)
        (cons "/")))
 
-(defn update-tree [tree path entries]
-  (update-in tree (path-sequence path) (constantly entries)))
+(defn update-tree
+  "Return a new tree with the item deeply nested at `path` replaced with
+   `value`"
+  [tree path value]
+  (update-in tree (path-sequence path) (constantly value)))
 
-(defn build-tree [[path tree] command]
+(defn add-to-tree
+  "Returns a new `[path, tree]` pair with the path reflecting any cwd changes,
+   and the tree reflecting any `ls` output from the given command"
+  [[path tree] command]
   (case (:command command)
     :cd (let [new-path (update-path path (:path command))]
           [new-path tree])
     :ls [path (update-tree tree path (:entries command))]))
 
-(defn get-size [tree]
+(defn get-size
+  "Return the cumulative size of a tree"
+  [tree]
   (if (number? tree) tree (:size tree)))
 
-(defn tree-size [tree]
+(defn tree-size
+  "Calculate the cumulative size of a tree. When the tree is a number, that
+   number is returned. Otherwise, a new tree is returned with a `:size`
+   property added."
+  [tree]
   (if (number? tree)
     tree
     (let [children (map (fn [[k v]] [k (tree-size v)]) tree)
@@ -63,21 +93,31 @@
           size (sum (map get-size (vals new-tree)))]
       (assoc new-tree :size size))))
 
-(defn find-directories [tree]
+(defn find-directories
+  "Returns a sequence of all the directory sizes contained in a tree"
+  [tree]
   (let [size (:size tree)]
     (if (nil? size)
       []
-      (cons size (mapcat find-directories (vals tree))))))
+      (->> (vals tree) (mapcat find-directories) (cons size)))))
+
+(defn commands-to-tree
+  "Converts a series of command objects into a tree data structure, and adds a
+   `:size` field to each directory with its cumulative size"
+  [commands]
+  (->> (reduce add-to-tree ["/" {}] commands)
+       (last)
+       (tree-size)))
 
 (defn part1 [input]
-  (let [tree (last (reduce build-tree ["/" {}] input))
-        tree (tree-size tree)]
-    (sum (filter #(< %1 100000) (find-directories (get tree "/"))))))
+  (->> (commands-to-tree input)
+       (find-directories)
+       (filter #(< %1 100000))
+       (sum)))
 
 (defn part2 [input]
-  (let [tree (last (reduce build-tree ["/" {}] input))
-        tree (tree-size tree)
-        dir-sizes (find-directories (get tree "/"))
+  (let [tree (commands-to-tree input)
+        dir-sizes (find-directories tree)
         used-space (:size tree)
         remaining-space (- total-space used-space)
         must-delete (- needed-space remaining-space)]
