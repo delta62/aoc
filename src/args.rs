817caa3ef0b2bfda0431b39bd3a8adcc1f00f9d4
Sam Noedel <sam.noedel@gmail.com>
2023-12-05 18:58:03 -0800

    Remove clap derive for faster builds & fewer deps

--- src/args.rs
+++
@@ -1,8 +0,0 @@
-use clap::Parser;
-
-#[derive(Debug, Parser)]
-pub struct Args {
-    /// Run all solutions. By default, only the latest day's solutions are run.
-    #[clap(short, long)]
-    pub all: bool,
-}
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,10 @@
 mod test_helpers;
 #[macro_use]
 mod input;
-mod args;
 mod days;
 
 use aoc_runner::{Runner, RunnerResult};
-use args::Args;
-use clap::Parser;
+use clap::{arg, command};
 use std::env;
 
 pub fn main() -> RunnerResult<()> {
@@ -19,13 +17,15 @@ pub fn main() -> RunnerResult<()> {
     }
 
     let session_token = env::var("AOC_SESSION").expect("No session token is set");
-    let args = Args::parse();
+    let args = command!()
+        .arg(arg!(-a --all "Run all solutions").required(false))
+        .get_matches();
 
     let runner = Runner::new()
         .auto_download_with_token(session_token)
         .with_input_path("input");
 
-    if args.all {
+    if args.get_flag("all") {
         runner.run_all()?;
     } else {
         runner.run_latest_day()?;
