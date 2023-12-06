deb9addbcef4f1e946b95590c23aa8c5df9feb45
Sam Noedel <sam.noedel@gmail.com>
2023-12-05 18:14:50 -0800

    Split runner out of solution files

--- aoc_runner_core/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![feature(iterator_try_collect)]
-
-mod error;
-mod solution;
-
-pub use error::{PuzzleError, Result};
-pub use inventory;
-pub use solution::{PuzzleAnswer, PuzzleInput, PuzzleSolution, Solution, SolutionOutput};
--- aoc_runner_macros/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod args;
-mod macros;
-
-use proc_macro::TokenStream;
-
-#[proc_macro_attribute]
-pub fn aoc(attr: TokenStream, item: TokenStream) -> TokenStream {
-    macros::aoc(attr, item)
-}
