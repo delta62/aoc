9df30540d3634d76b309456aa6b0a5da940084b3
Sam Noedel <sam.noedel@gmail.com>
2023-12-07 22:39:29 -0800

    Day 7 part 2

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,8 +1,10 @@
 use aoc_runner::{aoc, parse, parse_opt, PuzzleError, PuzzleInput, Result};
 use std::{
-    cmp::{Ordering, PartialEq},
+    cmp::Ordering,
     collections::{HashMap, HashSet},
 };
+
+const HAND_SIZE: usize = 5;
 
 #[aoc(year = 2023, day = 7, part = 1)]
 fn part1<'a>(mut input: Vec<Hand>) -> usize {
@@ -17,48 +19,76 @@ fn part1<'a>(mut input: Vec<Hand>) -> usize {
 
 #[aoc(year = 2023, day = 7, part = 2)]
 fn part2<'a>(mut input: Vec<Hand>) -> usize {
-    42
+    input
+        .iter_mut()
+        .for_each(|hand| hand.convert_jacks_to_jokers());
+
+    part1(input)
 }
 
 #[derive(Debug, Eq, Ord)]
 pub struct Hand {
     bid: usize,
     cards: Vec<Card>,
-    score: HandScore,
 }
 
 impl Hand {
     fn new(bid: usize, cards: Vec<Card>) -> Self {
-        let score = Self::score_hand(&cards);
-        Self { score, bid, cards }
+        Self { bid, cards }
     }
 
-    fn score_hand(cards: &[Card]) -> HandScore {
-        let groups = cards.iter().fold(HashMap::new(), |mut acc, card| {
-            let entry: &mut usize = acc.entry(card).or_default();
-            *entry += 1;
+    fn convert_jacks_to_jokers(&mut self) {
+        self.cards.iter_mut().for_each(|card| {
+            if card == &Card::Jack {
+                *card = Card::Joker;
+            }
+        });
+    }
+
+    fn score(&self) -> HandScore {
+        let mut joker_count = 0;
+        let groups = self.cards.iter().fold(HashMap::new(), |mut acc, card| {
+            if card == &Card::Joker {
+                joker_count += 1;
+            } else {
+                let entry: &mut usize = acc.entry(card).or_default();
+                *entry += 1;
+            }
+
             acc
         });
 
-        let mut group_sizes: Vec<_> = groups.values().collect();
-
+        let mut group_sizes: Vec<_> = groups.values().copied().collect();
         group_sizes.sort();
         group_sizes.reverse();
 
-        match group_sizes[0] {
+        let largest_group = group_sizes.first().copied().unwrap_or_default() + joker_count;
+        let second_largest_group = group_sizes.get(1).copied().unwrap_or_default();
+
+        match largest_group {
             5 => HandScore::FiveOfAKind,
             4 => HandScore::FourOfAKind,
-            3 => match group_sizes[1] {
+            3 => match second_largest_group {
                 2 => HandScore::FullHouse,
                 _ => HandScore::ThreeOfAKind,
             },
-            2 => match group_sizes[1] {
+            2 => match second_largest_group {
                 2 => HandScore::TwoPair,
                 _ => HandScore::OnePair,
             },
             1 => HandScore::HighCard,
             _ => unreachable!(),
         }
+    }
+}
+
+impl<'a> PuzzleInput<'a> for Hand {
+    fn parse(input: &'a str) -> Result<Self> {
+        let (cards, bid) = parse_opt!(input.split_once(' '), "expected space-separated input")?;
+        let bid = parse!(bid.parse::<usize>())?;
+        let cards = cards.chars().map(Card::try_from).try_collect::<Vec<_>>()?;
+
+        Ok(Self::new(bid, cards))
     }
 }
 
@@ -71,20 +101,10 @@ impl PartialEq for Hand {
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(
-            self.score
-                .cmp(&other.score)
+            self.score()
+                .cmp(&other.score())
                 .then(self.cards.cmp(&other.cards)),
         )
-    }
-}
-
-impl<'a> PuzzleInput<'a> for Hand {
-    fn parse(input: &'a str) -> Result<Self> {
-        let (cards, bid) = parse_opt!(input.split_once(' '), "expected space-separated input")?;
-        let bid = parse!(bid.parse::<usize>())?;
-        let cards = cards.chars().map(Card::try_from).try_collect::<Vec<_>>()?;
-
-        Ok(Self::new(bid, cards))
     }
 }
 
@@ -101,6 +121,7 @@ pub enum HandScore {
 
 #[derive(Debug, Clone, Copy, Ord, Eq, Hash, PartialEq, PartialOrd)]
 pub enum Card {
+    Joker,
     Two,
     Three,
     Four,
