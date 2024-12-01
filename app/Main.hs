3dbec44e3d5602e935672102488cb543df936ff2
Sam Noedel <sam.noedel@gmail.com>
2024-11-30 23:04:38 -0800

    Day 1 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Data.List (sort)
+import qualified Data.Set as S
+import qualified Data.Map as M
 
 parseStr :: String -> Int
 parseStr = read
@@ -14,14 +16,41 @@ parseLine s = tuple $ map parseStr $ words s
 parse :: String -> ([Int], [Int])
 parse s = unzip $ map parseLine (lines s)
 
+counts :: (Num a, Ord a, Enum a) => [a] -> M.Map a a
+counts xs = foldr (\x acc -> M.insertWith inc x 1 acc) M.empty xs
+  where
+    inc _ x = succ x
+
+parse' :: String -> (S.Set Int, M.Map Int Int)
+parse' s = (set, hash)
+  where
+    bits = unzip $ map parseLine (lines s)
+    set = S.fromList (fst bits)
+    hash = counts (snd bits)
+
 minDiffs :: ([Int], [Int]) -> Int
 minDiffs (xs, ys) = sum $ map (\(x, y) -> abs (x - y)) $ zip as bs
   where
     as = sort xs
     bs = sort ys
 
+scores :: M.Map Int Int -> Int
+scores m = M.foldrWithKey folder 0 m
+  where
+    folder k v acc = acc + k * v
+
+part1 :: String -> Int
+part1 input = minDiffs $ parse input
+
+part2 :: String -> Int
+part2 input = scores parsed'
+  where
+    parsed = parse' input
+    keys = fst parsed
+    parsed' = M.restrictKeys (snd parsed) keys
+
 main :: IO ()
 main = do
     input <- readFile "input/day01.txt"
-    let answer = minDiffs $ parse input
-    putStrLn $ show answer
+    putStrLn $ "Part 1: " ++ (show $ part1 input)
+    putStrLn $ "Part 2: " ++ (show $ part2 input)
