8a0ed6bfb1ad75c95f2d87c94f067ecc281af6c9
Sam Noedel <sam.noedel@gmail.com>
2024-11-30 21:46:38 -0800

    Day 1 part 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
+import Data.List (sort)
+
+parseStr :: String -> Int
+parseStr = read
+
+tuple :: [a] -> (a, a)
+tuple [x, y] = (x, y)
+
+parseLine :: String -> (Int, Int)
+parseLine s = tuple $ map parseStr $ words s
+
+parse :: String -> ([Int], [Int])
+parse s = unzip $ map parseLine (lines s)
+
+minDiffs :: ([Int], [Int]) -> Int
+minDiffs (xs, ys) = sum $ map (\(x, y) -> abs (x - y)) $ zip as bs
+  where
+    as = sort xs
+    bs = sort ys
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    input <- readFile "input/day01.txt"
+    let answer = minDiffs $ parse input
+    putStrLn $ show answer
